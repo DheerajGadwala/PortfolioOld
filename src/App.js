8bf8d62312bc06e88be2fcf52615128db375a81a
DheerajGadwala <65684290+DheerajGadwala@users.noreply.github.com>
2021-03-08 11:51:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MySidebar from './hooks/Sidebar/SidebarTransitioner'
+import './App.css'
+function App() {
+  return (
+    <MySidebar/>
+  );
+}
+
+export default App;
